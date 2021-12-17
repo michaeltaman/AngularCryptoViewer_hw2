168e7e815c808c140c45efe40a77bec68d8c832f
michaeltaman <42732094+michaeltaman@users.noreply.github.com>
2021-12-17 23:39:03 +0200

    Add files via upload

---
+++ client/src/app/services/coin.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Coin } from '../models/Coin';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoinService {
+
+  baseURL = `${environment.mainUrlAPI}`;
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Coin[]> {
+    return this.http.get<Coin[]>(this.baseURL).pipe(
+      map((rawResponse: any) => {
+        //console.log([...rawResponse.data]);
+        return [...rawResponse.data];
+    }));
+  }
+}
